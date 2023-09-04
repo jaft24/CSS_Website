f9b13dc4d56dc0266c5e2e7216c788aeb676ca83
jaft24 <jtesgera@cord.edu>
2023-09-03 20:21:36 -0500

    Called Drop Down

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Events from '../../components/Events/Events'
 import { VStack } from '@chakra-ui/react'
 import SignUp from '../../components/SignUp/SignUp'
 import HomeBanner from '../../components/HomeBanner/HomeBanner'
+import DropDown from '../../components/DropDown/DropDown'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <VStack gap={10} margin={'auto'} padding={10}>
       <HomeBanner />  
       <Events />
+      <DropDown />
       <SignUp />
       </VStack>
     </>
