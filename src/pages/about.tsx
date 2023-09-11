d47db4569d2ceef83043d9f7d19400c136192374
jaft24 <jtesgera@cord.edu>
2023-09-10 19:58:03 -0500

    Updated  Code

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,15 @@
+'use Client'
+
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 import "react-image-gallery/styles/scss/image-gallery.scss";
 import DropDown from '../../components/DropDown/DropDown';
-import { Text } from '@chakra-ui/react';
+import { Stack, Text } from '@chakra-ui/react';
+import AboutBanner from '../../components/AboutBanner/AboutBanner';
+import Members from '../../components/Members/Members';
+import Values from '../../components/Values/Values';
+import ContactUs from '../../components/ContactUs/ContactUs';
 
 
 export default function About() {
@@ -15,6 +21,10 @@ export default function About() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
+      <AboutBanner />
+
+
+      
     </>
   )
 }
